f0860c1a44a64d507146d12219b5ffd6d55b80de
xMurzik <murzik6667@gmail.com>
2023-09-27 17:27:51 +0300

    fix

--- src/components/rating-static/rating-static.tsx
+++ src/components/rating-static/rating-static.tsx
@@ -8,7 +8,7 @@ interface IRatingProps {
 
 const RatingStatic: React.FC<IRatingProps> = ({ rating }) =>
   RATING_ARR.map((el) => (
-    <svg data-testid="star" key={el} width={17} height={16} aria-hidden="true">
+    <svg key={el} data-testid="star" width={17} height={16} aria-hidden="true">
       <use xlinkHref={el <= rating ? '#icon-full-star' : '#icon-star'} />
     </svg>
   ));
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -3,14 +3,12 @@ import promoSlice from './promo-slice/promo-slice';
 import itemsSlice from './items-slice/items-slice';
 import itemSlice from './item-slice/item-slice';
 import modalSlice from './modal-slice/modal-slice';
-import basketSlice from './basket-slice/basket-slice';
 
 const rootReducer = combineReducers({
   promo: promoSlice,
   items: itemsSlice,
   item: itemSlice,
   modal: modalSlice,
-  basket: basketSlice,
 });
 
 export const store = configureStore({
