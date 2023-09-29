a44637a7fdd44e9b2b229c5e50b944e248406284
xMurzik <murzik6667@gmail.com>
2023-09-29 17:33:21 +0300

    fix modal focus

--- src/components/modal-item/modal-item.tsx
+++ src/components/modal-item/modal-item.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactFocusLock from 'react-focus-lock';
+import React, { useRef, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
 import {
   getCurrentActiveBasketItem,
@@ -33,103 +32,130 @@ const ModalItem: React.FC<IModalItemProps> = (props) => {
     }
   };
 
+  const refModalDiv = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Tab') {
+        const rootElement = refModalDiv.current;
+
+        if (rootElement) {
+          const focusableElements = rootElement.querySelectorAll(
+            'input, button, textarea, select'
+          );
+
+          const firstElement = focusableElements[0];
+          const lastElement = focusableElements[focusableElements.length - 1];
+
+          if (e.shiftKey && document.activeElement === firstElement) {
+            e.preventDefault();
+            (lastElement as HTMLElement).focus();
+          } else if (!e.shiftKey && document.activeElement === lastElement) {
+            e.preventDefault();
+            (firstElement as HTMLElement).focus();
+          }
+        }
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   if (!data) {
     return null;
   }
 
   return (
-    <ReactFocusLock>
-      <div className={`modal ${isActive ? 'is-active' : ''}`}>
-        <div className="modal__wrapper">
-          <div onClick={onClickExit} className="modal__overlay" />
-          <div className="modal__content">
-            <p className="title title--h4">
-              {isDelete ? 'Удалить этот товар?' : 'Добавить товар в корзину'}
-            </p>
-            <div className="basket-item basket-item--short">
-              <div className="basket-item__img">
-                <picture>
-                  <source
-                    type="image/webp"
-                    srcSet={`${data.previewImgWebp}, ${data.previewImgWebp2x} 2x`}
-                  />
-                  <img
-                    src={data.previewImg}
-                    srcSet={`${data.previewImg2x} 2x`}
-                    width={140}
-                    height={120}
-                    alt={data.name}
-                  />
-                </picture>
-              </div>
-              <div className="basket-item__description">
-                <p className="basket-item__title">{data.name}</p>
-                <ul className="basket-item__list">
-                  <li className="basket-item__list-item">
-                    <span className="basket-item__article">Артикул:</span>{' '}
-                    <span className="basket-item__number">
-                      {data.vendorCode}
-                    </span>
-                  </li>
-                  <li className="basket-item__list-item">
-                    {data.type} фотокамера
-                  </li>
-                  <li className="basket-item__list-item">
-                    {data.level} уровень
-                  </li>
-                </ul>
-                {!isDelete && (
-                  <p className="basket-item__price">
-                    <span className="visually-hidden">Цена:</span>
-                    {data.price} ₽
-                  </p>
-                )}
-              </div>
+    <div ref={refModalDiv} className={`modal ${isActive ? 'is-active' : ''}`}>
+      <div className="modal__wrapper">
+        <div onClick={onClickExit} className="modal__overlay" />
+        <div className="modal__content">
+          <p className="title title--h4">
+            {isDelete ? 'Удалить этот товар?' : 'Добавить товар в корзину'}
+          </p>
+          <div className="basket-item basket-item--short">
+            <div className="basket-item__img">
+              <picture>
+                <source
+                  type="image/webp"
+                  srcSet={`${data.previewImgWebp}, ${data.previewImgWebp2x} 2x`}
+                />
+                <img
+                  src={data.previewImg}
+                  srcSet={`${data.previewImg2x} 2x`}
+                  width={140}
+                  height={120}
+                  alt={data.name}
+                />
+              </picture>
             </div>
-            {isDelete ? (
-              <div className="modal__buttons">
-                <button
-                  className="btn btn--purple modal__btn modal__btn--half-width"
-                  type="button"
-                >
-                  Удалить
-                </button>
-                <a
-                  className="btn btn--transparent modal__btn modal__btn--half-width"
-                  href="#"
-                >
-                  Продолжить покупки
-                </a>
-              </div>
-            ) : (
-              <div className="modal__buttons">
-                <button
-                  className="btn btn--purple modal__btn modal__btn--fit-width"
-                  type="button"
-                  id="add-item-to-basket"
-                  onClick={onClickButtonSuccessBuy}
-                >
-                  <svg width="24" height="16" aria-hidden="true">
-                    <use xlinkHref="#icon-add-basket"></use>
-                  </svg>
-                  Добавить в корзину
-                </button>
-              </div>
-            )}
-            <button
-              className="cross-btn"
-              type="button"
-              aria-label="Закрыть попап"
-              onClick={onClickExit}
-            >
-              <svg width={10} height={10} aria-hidden="true">
-                <use xlinkHref="#icon-close" />
-              </svg>
-            </button>
+            <div className="basket-item__description">
+              <p className="basket-item__title">{data.name}</p>
+              <ul className="basket-item__list">
+                <li className="basket-item__list-item">
+                  <span className="basket-item__article">Артикул:</span>{' '}
+                  <span className="basket-item__number">{data.vendorCode}</span>
+                </li>
+                <li className="basket-item__list-item">
+                  {data.type} фотокамера
+                </li>
+                <li className="basket-item__list-item">{data.level} уровень</li>
+              </ul>
+              {!isDelete && (
+                <p className="basket-item__price">
+                  <span className="visually-hidden">Цена:</span>
+                  {data.price} ₽
+                </p>
+              )}
+            </div>
           </div>
+          {isDelete ? (
+            <div className="modal__buttons">
+              <button
+                className="btn btn--purple modal__btn modal__btn--half-width"
+                type="button"
+              >
+                Удалить
+              </button>
+              <a
+                className="btn btn--transparent modal__btn modal__btn--half-width"
+                href="#"
+              >
+                Продолжить покупки
+              </a>
+            </div>
+          ) : (
+            <div className="modal__buttons">
+              <button
+                className="btn btn--purple modal__btn modal__btn--fit-width"
+                type="button"
+                id="add-item-to-basket"
+                onClick={onClickButtonSuccessBuy}
+              >
+                <svg width="24" height="16" aria-hidden="true">
+                  <use xlinkHref="#icon-add-basket"></use>
+                </svg>
+                Добавить в корзину
+              </button>
+            </div>
+          )}
+          <button
+            className="cross-btn"
+            type="button"
+            aria-label="Закрыть попап"
+            onClick={onClickExit}
+          >
+            <svg width={10} height={10} aria-hidden="true">
+              <use xlinkHref="#icon-close" />
+            </svg>
+          </button>
         </div>
       </div>
-    </ReactFocusLock>
+    </div>
   );
 };
 
